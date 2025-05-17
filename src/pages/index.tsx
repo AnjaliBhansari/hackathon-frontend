08ad83d84e9640202fb86dd14b2e8d7a1e8a68de
AnjaliBhansari <131841801+AnjaliBhansari@users.noreply.github.com>
2025-05-17 13:02:52 +0530

    fix: changes done for login and signup
    Signed-off-by: AnjaliBhansari <131841801+AnjaliBhansari@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { HomeView } from '@/presentation/features/home';
+import React from "react";
 
 /**
  * Next.js index page that serves as the entry point for the home route.
  * This page uses the HomeView component from our presentation layer.
  */
 export default function Home() {
-  return <HomeView />;
-} 
+  return <>hi</>;
+}
